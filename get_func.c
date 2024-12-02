9acc710b1d190ced923552b52462174bcd394091
TommyBryan <tommybryan0713@gmail.com>
2024-12-01 20:18:37 -0400

    Made changes

--- get_func.c
+++ get_func.c
@@ -6,22 +6,25 @@
  * 
  * Return: Pointer to the function or NULL if invalid.
  */
+
 int (*get_func(char x))(va_list)
 {
-    int i = 0;
-    spec arr[] = {
-        {'c', print_c},
-        {'s', print_s},
-        {'%', print_percent},
-        {'\0', NULL}
-    };
+	int i = 0;
 
-    while (arr[i].valid)
-    {
-        if (x == arr[i].valid)
-            return (arr[i].f);
-        i++;
-    }
-    return (NULL);
+	spec arr[] = {
+		{'c', print_c},
+		{'s', print_s},
+		{'%', print_percent},
+		{'\0', NULL}
+	};
+
+	while (arr[i].valid)
+	{
+		if (x == arr[i].valid)
+			return (arr[i].f);
+		i++;
+	}
+
+	return (NULL);
 }
 
